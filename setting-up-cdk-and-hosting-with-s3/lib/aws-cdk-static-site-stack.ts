9a550ab81e93fa294737598c1622730342c4bc95
Matthew Murray <mattmurr.uk@gmail.com>
2021-08-22 21:23:47 +0100

    No need for two snapshots for a single post

--- setting-up-cdk-and-hosting-with-s3/lib/aws-cdk-static-site-stack.ts
+++ setting-up-cdk-and-hosting-with-s3/lib/aws-cdk-static-site-stack.ts
@@ -1,6 +1,8 @@
 import * as cdk from '@aws-cdk/core';
 import * as s3 from "@aws-cdk/aws-s3";
 import * as s3Deploy from "@aws-cdk/aws-s3-deployment";
+import * as cloudfront from "@aws-cdk/aws-cloudfront";
+import * as origins from "@aws-cdk/aws-cloudfront-origins";
 
 export class AwsCdkStaticSiteStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -8,18 +10,30 @@ export class AwsCdkStaticSiteStack extends cdk.Stack {
 
     const bucket = new s3.Bucket(this, "Bucket", {
       bucketName: "mattmurr-aws-cdk-static-site",
-      websiteIndexDocument: "index.html",
-      publicReadAccess: true,
       autoDeleteObjects: true,
       removalPolicy: cdk.RemovalPolicy.DESTROY
     });
 
-    new s3Deploy.BucketDeployment(this, "BucketDeployment", {
-      // Source our site files from `./site` dir
-      sources: [s3Deploy.Source.asset("./site")],
-      destinationBucket: bucket,
+    new cdk.CfnOutput(this, "BucketURL", { value: bucket.bucketWebsiteUrl });
+
+    const distribution = new cloudfront.Distribution(this, "SiteDistribution", {
+      defaultBehavior: {
+        origin: new origins.S3Origin(bucket),
+        viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+      },
+      defaultRootObject: "index.html",
     });
 
-    new cdk.CfnOutput(this, "BucketURL", { value: bucket.bucketWebsiteUrl });
+    new s3Deploy.BucketDeployment(this, "BucketDeployment", {
+      sources: [s3Deploy.Source.asset("./site")],
+      destinationBucket: bucket,
+      distribution,
+      distributionPaths: ["/*"]
+    });
+
+    new cdk.CfnOutput(this, "DistributionDomainName", {
+      value: "https://" + distribution.domainName,
+    });
+
   }
 }
