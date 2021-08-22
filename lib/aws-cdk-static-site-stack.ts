289ff0657fa20071d53959de4aeed4b5773a362b
Matthew Murray <mattmurr.uk@gmail.com>
2021-08-22 18:02:21 +0100

    Part-1

--- lib/aws-cdk-static-site-stack.ts
+++ lib/aws-cdk-static-site-stack.ts
@@ -1,9 +1,25 @@
 import * as cdk from '@aws-cdk/core';
+import * as s3 from "@aws-cdk/aws-s3";
+import * as s3Deploy from "@aws-cdk/aws-s3-deployment";
 
 export class AwsCdkStaticSiteStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const bucket = new s3.Bucket(this, "Bucket", {
+      bucketName: "aws-cdk-static-site",
+      websiteIndexDocument: "index.html",
+      publicReadAccess: true,
+      autoDeleteObjects: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY
+    });
+
+    new s3Deploy.BucketDeployment(this, "BucketDeployment", {
+      // Source our site files from `./site` dir
+      sources: [s3Deploy.Source.asset("./site")],
+      destinationBucket: bucket,
+    });
+
+    new cdk.CfnOutput(this, "BucketURL", { value: bucket.bucketWebsiteUrl });
   }
 }
